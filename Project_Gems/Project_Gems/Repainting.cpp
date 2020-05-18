a9ea9a8e898d32b6430be808a2c421d53f5ac2ed
Anastasia <60839846+Brightest-Sunshine@users.noreply.github.com>
2020-05-18 16:49:30 +0500

    Update Repainting.cpp

--- Project_Gems/Project_Gems/Repainting.cpp
+++ Project_Gems/Project_Gems/Repainting.cpp
@@ -10,15 +10,15 @@ void Repainting::DrawBonusTexture(std::shared_ptr<sf::RenderWindow> window, std:
 	tex.loadFromFile("images/painter.png");
 	painter.setTexture(tex);
 
-	field->_gemsArray[x][y] = field->TRANSPARENT;
+	field->DoTransparent(x, y);
 
 	for (int i = 0; i < 25; i++) {
 		field->DrawField(*window);
 		window->display();
 	}
-	
-	painter.setPosition(float(x * (field->_gemSize + field->_indent) + field->_startPoint), float(y * (field->_gemSize + field->_indent) + field->_startPoint));
-	
+
+	painter.setPosition(float(x * (field->GetGemSize() + field->GetIndent()) + field->GetStartPoint()), float(y * (field->GetGemSize() + field->GetIndent()) + field->GetStartPoint()));
+
 	for (int i = 0; i < 40; i++) {
 		window->draw(painter);
 		window->display();
@@ -30,7 +30,7 @@ int Repainting::Actuation(std::shared_ptr<Field> field)
 {
 	int new_color, tmp1, tmp2, tmp_x, tmp_y;
 	new_color = rand() % 7;
-	field->_gemsArray[x][y] = new_color;
+	field->ChangeColor(x, y, new_color);
 
 	for (int i = 0; i < countPaintedGems; i++) {
 		tmp1 = rand() % paintingRadius + 1;
@@ -43,7 +43,7 @@ int Repainting::Actuation(std::shared_ptr<Field> field)
 			tmp_y = y - tmp2;
 		else
 			tmp_y = y + tmp2;
-		field->_gemsArray[tmp_x][tmp_y] = new_color;
+		field->ChangeColor(tmp_x, tmp_y, new_color);
 	}
 	return 0;
 }
